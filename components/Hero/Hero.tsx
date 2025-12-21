8cf12957badaeadcc5ff7e9c76d143d8ec937809
ahmadshaker1 <74874913+ahmadshaker1@users.noreply.github.com>
2025-12-21 12:58:18 +0200

    Added blur effect on hero section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import { Button } from "@headlessui/react";
 const Hero = () => {
   return (
     <div
-      className="flex flex-col items-start justify-center h-screen w-screen px-24 space-y-24"
+      className="flex flex-col items-start justify-center h-screen w-screen px-24 space-y-16 overflow-hidden"
       style={{
         backgroundImage: `url('/assets/landing/discover-aseer-hero.jpg')`,
         backgroundSize: "fill",
@@ -11,12 +11,27 @@ const Hero = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <h1 className="text-8xl font-bold text-white">ألف مرحبا بكم في عسير</h1>
-      <h3 className="text-2xl font-bold text-white">
-        حيث تلتقي التقاليد الخالدة بالمناظر الطبيعية الخلابة. جرب مزيجا فريدا من
-        الثقافة والمغامرة والجمال الذي لا مثيل له.
-      </h3>
-      <Button className="border-2 border-white text-white px-8 py-4 cursor-pointer rounded-full bg-white/10 backdrop-blur-md hover:bg-white/20 transition-all duration-300 shadow-lg">
+      <div className="relative py-8 px-12">
+        <div
+          className="absolute bg-black/10 backdrop-blur-xs"
+          style={{
+            borderRadius: "50%",
+            width: "1800px",
+            height: "1200px",
+            right: "-50%",
+            top: "-200%",
+            filter: "blur(1px)",
+          }}
+        ></div>
+        <h1 className="relative text-8xl font-bold text-white z-10">
+          ألف مرحبا بكم في عسير
+        </h1>
+        <h3 className="relative text-2xl font-bold text-white z-10 mt-4">
+          حيث تلتقي التقاليد الخالدة بالمناظر الطبيعية الخلابة. جرب مزيجا فريدا
+          من الثقافة والمغامرة والجمال الذي لا مثيل له.
+        </h3>
+      </div>
+      <Button className="border-2 border-white text-white px-8 py-3 cursor-pointer rounded-full bg-white/10 backdrop-blur-md hover:bg-white/20 transition-all duration-300 shadow-lg">
         شاهد الفيلم
       </Button>
     </div>
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import AseerLogo from "../Logo/AseerLogo";
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-row items-center justify-between absolute top-0 left-0 w-full h-24 z-10 bg-[#191919]/32 backdrop-blur-md">
+      <div className="flex flex-row items-center justify-between w-full max-w-7xl mx-auto">
+        <AseerLogo />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
