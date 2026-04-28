faed010d83aadb76deafa6819913d1399860d040
Mohammed Batis <mohammed.batis.dev@gmail.com>
2026-04-28 13:55:42 +0300

    IGCAT in hold 50% done

--- app/igcat/page.tsx
+++ app/igcat/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import IGCatBannerSection from "@/components/igcat/bannerSection";
 import IGCatBackgroundSection from "@/components/igcat/backgroundSection";
+import IGCatEventCards from "@/components/igcat/eventCards";
+import QuotesSection from "@/components/igcat/quotesSection";
 export default function IGCatPage() {
   return (
     <main className="flex flex-col w-full">
       <IGCatBannerSection />
       <IGCatBackgroundSection />
+      <IGCatEventCards />
+      <QuotesSection />
     </main>
   );
 }
