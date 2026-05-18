d4dba112d5294d217cd8da2b9e74705139cdcca3
ahmadshaker1 <74874913+ahmadshaker1@users.noreply.github.com>
2026-05-18 13:25:27 +0300

    Fixed the the locations in the about aseer page and their images

--- components/about-aseer/AboutAseerPage.tsx
+++ components/about-aseer/AboutAseerPage.tsx
@@ -1,17 +1,10 @@
-import { getLocale, getTranslations } from "next-intl/server";
+import { getTranslations } from "next-intl/server";
 import AboutAseerHero, {
   type AboutAseerHeroData,
 } from "@/components/about-aseer/AboutAseerHero";
 import AboutAseerStorySection, {
   type AboutStoryContent,
 } from "@/components/about-aseer/AboutAseerStorySection";
-import {
-  resolveAboutHighlightLandmark,
-  type AboutHighlightId,
-} from "@/components/about-aseer/aboutHighlightLandmarks";
-import { fetchLandmarks } from "@/components/landmarks/data";
-
-const HIGHLIGHT_IDS: AboutHighlightId[] = ["h-1", "h-2", "h-3", "h-4"];
 const HIGHLIGHT_TITLE_KEYS = [
   "story.highlights.h1",
   "story.highlights.h2",
@@ -19,21 +12,22 @@ const HIGHLIGHT_TITLE_KEYS = [
   "story.highlights.h4",
 ] as const;
 
+const HIGHLIGHT_IMAGES = [
+  "/assets/aboutAseer/mountains.png",
+  "/assets/aboutAseer/plains.png",
+  "/assets/aboutAseer/beaches.jpg",
+  "/assets/aboutAseer/desert.jpg",
+] as const;
+
 const AboutAseerPage = async () => {
-  const locale = (await getLocale()) as "ar" | "en";
   const t = await getTranslations("aboutAseer");
   const tCommon = await getTranslations("common");
-  const landmarks = await fetchLandmarks(locale);
 
-  const highlightCards = HIGHLIGHT_IDS.map((id, index) => {
-    const landmark = resolveAboutHighlightLandmark(id, landmarks);
-    return {
-      id,
-      title: t(HIGHLIGHT_TITLE_KEYS[index]),
-      image: landmark?.image ?? "/assets/aboutAseer/about_aseer_banner.webp",
-      href: landmark ? `/attractions/${landmark.slug}` : undefined,
-    };
-  });
+  const highlightCards = HIGHLIGHT_TITLE_KEYS.map((titleKey, index) => ({
+    id: `h-${index + 1}`,
+    title: t(titleKey),
+    image: HIGHLIGHT_IMAGES[index],
+  }));
 
   const aboutPageData: {
     hero: AboutAseerHeroData;
